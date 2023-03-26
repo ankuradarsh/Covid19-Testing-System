0d460420d553f0dd030adc5cfb090a3de016282d
Ankur Adarsh <ankuradarsh123@gmail.com>
2023-03-26 10:33:05 +0530

    first commit

---
+++ dashboard.php
@@ -0,0 +1,328 @@
+<?php
+session_start();
+include_once('includes/config.php');
+if (strlen($_SESSION['aid']==0)) {
+  header('location:logout.php');
+  } else{
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Covid-19 | Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+       <?php include_once('includes/sidebar.php');?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+<?php include_once('includes/topbar.php');?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <a href="bwdates-report-ds.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="row">
+
+<?php 
+//Total tests
+$query=mysqli_query($con,"select id from tbltestrecord");
+$totaltest=mysqli_num_rows($query);
+//Assigned tests
+$query1=mysqli_query($con,"select id from tbltestrecord where ReportStatus='Assigned'");
+$totalassigned=mysqli_num_rows($query1);
+//On the way for sample collection
+$query2=mysqli_query($con,"select id from tbltestrecord where ReportStatus='On the Way for Collection'");
+$totalontheway=mysqli_num_rows($query2);
+//Sample Collected
+$query3=mysqli_query($con,"select id from tbltestrecord where ReportStatus='Sample Collected'");
+$totalsamplecollected=mysqli_num_rows($query3);
+//Sent for lab
+$query4=mysqli_query($con,"select id from tbltestrecord where ReportStatus='Sent to Lab'");
+$totalsenttolab=mysqli_num_rows($query4);
+
+//Report Delivered
+$query5=mysqli_query($con,"select id from tbltestrecord where ReportStatus='Delivered'");
+$totaldelivered=mysqli_num_rows($query5);
+
+//Totall Registered Patients 
+$query6=mysqli_query($con,"select id from tblpatients");
+$totalpatients=mysqli_num_rows($query6); 
+
+//Totall Registered Phlebotomist 
+$query7=mysqli_query($con,"select id from tblphlebotomist");
+$totalphlebotomist=mysqli_num_rows($query7);
+?>
+
+
+                        <!-- Total Tests-->
+                        <div class="col-xl-2 col-md-2"></div>
+                        <div class="col-xl-8 col-md-8 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-4">
+                                <a href="all-test.php">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-lg font-weight-bold text-primary text-uppercase mb-1">
+                                             Total Tests</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totaltest;?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-virus fa-3x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-md-2"></div>
+
+                        <!-- Assigned test-->
+                        <div class="col-xl-2 col-md-2"></div>
+                        <div class="col-xl-8 col-md-8 mb-4">
+                            <div class="card border-left-success shadow h-100 py-4">
+                                <a href="assigned-test.php">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-lg font-weight-bold text-success text-uppercase mb-1">
+                                              Total Assigned</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totalassigned;?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-user fa-3x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-md-2"></div>
+
+                        <!-- On the way for sample collection-->
+                        <div class="col-xl-2 col-md-2"></div>
+                        <div class="col-xl-8 col-md-8 mb-4">
+                            <div class="card border-left-info shadow h-100 py-4">
+                                <a href="ontheway-samplecollection-test.php">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-lg font-weight-bold text-info text-uppercase mb-1"> On the way for sample collection
+                                            </div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $totalontheway;?></div>
+                                                </div>
+                                                <div class="col">
+                                                 
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-motorcycle fa-3x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-md-2"></div>
+
+                        <!-- Sample Collected -->
+                        <div class="col-xl-2 col-md-2"></div>
+                        <div class="col-xl-8 col-md-8 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-4">
+                                <a href="sample-collected-test.php">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-lg font-weight-bold text-warning text-uppercase mb-1">
+                                               Sample Collected</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totalsamplecollected;?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-prescription-bottle fa-3x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-md-2"></div>
+
+                        <!-- Sent to Lab -->
+                        <div class="col-xl-2 col-md-2"></div>
+                        <div class="col-xl-8 col-md-8 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-4">
+                                <a href="samplesent-lab-test.php">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-lg font-weight-bold text-warning text-uppercase mb-1">
+                                              Sample Sent to Lab</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totalsenttolab;?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-microscope fa-3x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-md-2"></div>
+
+                        <!-- Report Delivered-->
+                        <div class="col-xl-2 col-md-2"></div>
+                        <div class="col-xl-8 col-md-8 mb-4">
+                            <div class="card border-left-info shadow h-100 py-4">
+                                <a href="reportdelivered-test.php">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-lg font-weight-bold text-info text-uppercase mb-1">Report Delivered
+                                            </div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $totaldelivered;?></div>
+                                                </div>
+                                                <div class="col">
+                                                 
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-3x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-md-2"></div>
+
+                        <!-- Total Registered Patients-->
+                        <div class="col-xl-2 col-md-2"></div>
+                        <div class="col-xl-8 col-md-8 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-4">
+
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+
+
+                                            <div class="text-lg font-weight-bold text-primary text-uppercase mb-1">
+                                             Total Registered Patients</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totalpatients;?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-users fa-3x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-md-2"></div>
+
+                        <!-- Total Registered Phlebotomist-->
+                        <div class="col-xl-2 col-md-2"></div>
+                        <div class="col-xl-8 col-md-8 mb-4">
+                            <div class="card border-left-success shadow h-100 py-4">
+                                      <a href="manage-phlebotomist.php">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                   
+                                        <div class="col mr-2">
+                                            <div class="text-lg font-weight-bold text-success text-uppercase mb-1">
+                                              Total Registered Phlebotomist</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totalphlebotomist;?></div>
+                                        </div>
+
+                                        <div class="col-auto"> 
+                                            <i class="fas fa-user-nurse fa-3x text-gray-300"></i>
+                                        </div> 
+                                    </div>
+                                </div>
+                                 </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-md-2"></div>
+
+                    <!-- Content Row -->
+
+              
+
+             
+
+            <!-- Footer -->
+       <?php include_once('includes/footer.php');?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+           <?php include_once('includes/footer2.php');?>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
+<?php } ?>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Co-Knox 2021 All right reserved.</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
